4ff9fba2e4c61967dcc837cac0632cd9643d1c55
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-20 22:55:13 +0200

    feat: models/MenuItem.js file created

---
+++ src/models/MenuItem.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const MenuItemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    desc: { type: String, default: "" },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+
+    isVeg: { type: Boolean, default: false },
+    available: { type: Boolean, default: true },
+    featured: { type: Boolean, default: false },
+
+    imageUrl: { type: String, default: "" },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model("MenuItem", MenuItemSchema);
