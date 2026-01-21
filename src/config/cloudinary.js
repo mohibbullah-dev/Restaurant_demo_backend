d1d320c2966c0ef02d4613699b470f6afd6729da
Md Mohibbullah <mohammedhanif74612@gmail.com>
2026-01-21 13:35:07 +0200

    modi: cloudinary.js is modified

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -1,9 +1,13 @@
 import { v2 as cloudinary } from "cloudinary";
+console.log(process.env.CLOUDINARY_CLOUD_NAME);
+console.log(process.env.CLOUDINARY_API_KEY);
+console.log(process.env.CLOUDINARY_API_SECRET);
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "dwcbvfptd",
+  api_key: process.env.CLOUDINARY_API_KEY || "494153674418449",
+  api_secret:
+    process.env.CLOUDINARY_API_SECRET || "r1R--jPuWMBs8AYobc6PyJQbazQ",
 });
 
 export default cloudinary;
